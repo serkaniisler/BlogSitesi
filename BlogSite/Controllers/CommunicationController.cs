1aebb3c659a2a31011f83720f6bbadcf2c1496ba
Serkan <serkaniisler@gmail.com>
2025-04-15 17:59:42 +0300

    Added CommunicationController and Contact view page under the Communication folder.

---
+++ BlogSite/Controllers/CommunicationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite.Controllers
+{
+    public class CommunicationController : Controller
+    {
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
